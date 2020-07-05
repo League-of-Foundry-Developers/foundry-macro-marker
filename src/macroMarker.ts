1487e8b445fbfff7c2bf4cd6e7f129f0ca5e40ca
Stan Janssen <mail@janssen.io>
2020-07-05 20:58:31 +0200

    Added activate, deactivate and toggle

--- src/macroMarker.ts
+++ src/macroMarker.ts
@@ -5,6 +5,12 @@ import { Settings } from './settings';
 import { Logger } from './logger';
 import { MarkerCleaner } from './markerCleaner';
 
+interface ToggleData {
+    token?: Token & Flaggable,
+    user?: Flaggable,
+    colour?: string
+}
+
 export class MacroMarker {
     constructor(
         private logger: Logger,
@@ -78,11 +84,46 @@ export class MacroMarker {
         return this._toggleMacro(macro, new MarkerFlags(this.logger, macro), colour);
     }
     
-    public isActive(macro: Macro, flag: Flaggable): boolean {
-        const markers = new MarkerFlags(this.logger, flag);
+    public isActive(macro: Macro & Flaggable, data?: { token?: Token & Flaggable, user?: Flaggable }): boolean {
+        let entity: Flaggable = macro;
+        if (data?.token)
+            entity = data.token.data.actorLink && data.token.actor
+                ? data.token.actor
+                : data.token;
+        else if (data?.user)
+            entity = data.user;
+
+        const markers = new MarkerFlags(this.logger, entity);
         return markers.getMarkers()[macro.id]?.active || false;
     }
 
+    public toggle(macro: Macro & Flaggable, data?: ToggleData): Promise<Flaggable> {
+        if (data?.token && data.user) {
+            ui.notifications.warn('Markers cannot be set on both tokens and users.');
+            Promise.reject();
+        }
+
+        if (data?.token)
+            return this.toggleTokenMacro(macro, data.token, data.colour);
+        else if(data?.user)
+            return this.toggleUserMacro(macro, data.user, data.colour);
+        else
+            return this.toggleMacro(macro, data?.colour);
+    }
+
+    public activate(macro: Macro & Flaggable, data?: ToggleData): Promise<Flaggable> {
+        if (this.isActive(macro, data))
+            return Promise.resolve(macro);
+
+        return this.toggle(macro, data);
+    }
+
+    public deactivate(macro: Macro & Flaggable, data?: ToggleData): Promise<Flaggable> {
+        if (!this.isActive(macro, data))
+            return Promise.resolve(macro);
+
+        return this.toggle(macro, data);
+    }
 
     private _toggleMacro(macro: Macro, flags: MarkerFlags, colour?: string): Promise<Flaggable>{
         const existingMarker: Marker | undefined = flags.getMarkers()[macro.id];
